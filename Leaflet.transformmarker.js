fd663510216ce17dcaa2360aa6f1adb5fa5ac371
Richard Chirino <richard.chirino@ozomedia.nl>
2015-11-10 13:05:41 +0100

    Update Leaflet.transformmarker.js

--- Leaflet.transformmarker.js
+++ Leaflet.transformmarker.js
@@ -13,7 +13,7 @@ L.TransformMarker = L.Marker.extend({
 
 		if (L.Browser.any3d && this._transform) {
 			this._icon.style[L.DomUtil.TRANSFORM] += this._transform;
-			this._icon.style['transformOrigin'] = (this._icon.x * -1) + 'px ' + (this._icon.y * -1) + 'px';
+			this._icon.style['transformOrigin'] = (this._icon.width / 2) + 'px ' + (this._icon.height / 2) + 'px';
 		}
 	}
 
